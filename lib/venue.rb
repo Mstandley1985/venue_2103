87449590eea895e86c922fd4922413ec5e68f0c8
Mitch Standley <mitch_standley@yahoo.com>
2021-03-24 14:00:02 -0700

    Complete iteration 1 and 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,24 @@
 class Venue
+  attr_reader :name, :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+
+  end
+
+  def capacity
+    4
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map(&:upcase)
+  end
+
+
+
 end
